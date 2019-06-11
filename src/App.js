c103f8495841145f65584ff622d8d26ba9d5fd33
Dave <oldmcdonald1@gmail.com>
2019-06-11 18:33:38 +1000

    connected app.js to server API and managed CORS

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React from 'react';
 
 class App extends React.Component {
+  state = {
+    todolist: null
+  }
+
+  componentDidMount() {
+    const url = 'http://localhost:8080/api/todolist'
+    fetch(url)
+    .then(res => res.json())
+    .then(data => this.setState({ todolist: data }))
+  }
+
   render() {
     return (
       <div className="App">
 
         <div>
           <form className="todoitem" action="">
-            <h1>Add a todo item</h1>
-            <p>Description</p>
+            <h2>Add a todo item:</h2>
+            <p>Title</p>
             <input type="text" placeholder="fix fence"/>
             <input type="submit" value="Submit"/>
           </form>
         </div>
 
+        <h2>Current todo list:</h2>
+        <p>...</p>
       </div>
     );
   }
