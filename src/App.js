bf6d32eae0580278fd4f66ade90a8801eff884d1
Dave <oldmcdonald1@gmail.com>
2019-06-12 10:26:01 +1000

    handleInputField grabs temporary input ready to add to item list.

--- src/App.js
+++ src/App.js
@@ -2,23 +2,46 @@ import React from 'react';
 import ToDoList from './ToDoList'
 
 class App extends React.Component {
-  state = {
-    todolist: null
+  constructor() {
+    super()
+    this.state = {
+      items: [], // to do list items
+      currentItem: {text:'', key:''} // store text entered
+    }
   }
 
+  // Get data from server api call
   componentDidMount() {
     const apiUrl = 'http://localhost:8080/api/todolist'
-    fetch(apiUrl)
-    .then(res => res.json())
-    .then(data => this.setState({ 
-      todolist: data
+    fetch(apiUrl) // fetch api data
+    .then(res => res.json()) // convert api data to json
+    .then(data => this.setState({ // add db items to state (overwrites currently)
+      items: data
     }))
+  }
+
+  // get input text ready to store when form submitted
+  handleInputField = event => {
+    console.log('input received')
+    const itemText = event.target.value // get text in field
+    const currentItem = { text: itemText, key: Date.now() }
+    this.setState({currentItem}) // store 
+  }
+
+  addItem = event => {
+    event.preventDefault()
+    console.log('adding item to state')
   }
 
   render() {
     return (
       <div className="App">
-        <ToDoList />
+        <ToDoList
+          addItem={this.addItem}
+          inputElement = {this.inputElement}
+          handleInputField={this.handleInputField}
+          currentItem={this.state.currentItem}
+        />
         <h2>Current todo list:</h2>
         <p>...</p>
       </div>
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,16 +1,24 @@
 import React from 'react'
-
 export default class ToDoList extends React.Component {
+
   render() {
     return (
-      <div>
-      <form className="todoitem" action="">
+      <div className="toDoList">
         <h2>Add a todo item:</h2>
         <p>Title</p>
-        <input type="text" placeholder="fix fence"/>
-        <input type="submit" value="Add Task"/>
-      </form>
-    </div>
+
+        <form action="" onSubmit={this.props.addItem}>
+          <input
+            type="text"
+            placeholder="fix fence"
+            ref={this.props.inputElement}
+            value={this.props.currentItem.text}
+            onChange={this.props.handleInputField}
+          />
+          <button type="submit">Add Task</button>
+        </form>
+
+      </div>
     )
   }
 }
