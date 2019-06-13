bf1fc123b0dcb7049da6f5172d80069a0ad6874e
Dave <oldmcdonald1@gmail.com>
2019-06-13 17:32:34 +1000

    remove background sync from index.js

---
+++ build/precache-manifest.f008d69850c6505f75ae344208e299d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0023e9b3c97a4d4034de2176c81e9f44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4680e72174d96767e246",
+    "url": "/static/css/main.69e33441.chunk.css"
+  },
+  {
+    "revision": "543b7572bbec57b543d2",
+    "url": "/static/js/2.121ed12e.chunk.js"
+  },
+  {
+    "revision": "4680e72174d96767e246",
+    "url": "/static/js/main.9c0fb181.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
